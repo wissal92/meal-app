f9a90bbc778646b421f66f870d25fd82a0c7e7ec
wissal <wissal.ouarsafi@gmail.com>
2020-04-02 18:17:48 -0700

    Create get random recipe functionality

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function searchMeal(e){
 function getMealById(mealID){
     mealsEl.innerHTML = '';
     resultHeading.innerHTML = '';
-    
+
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
        .then(res => res.json())
        .then(data => {
@@ -86,9 +86,23 @@ function addMealToDOM(meal){
     `;
 }
 
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+  
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      .then(res => res.json())
+      .then(data => {
+        const meal = data.meals[0];
+  
+        addMealToDOM(meal);
+    });
+}
+
 // Event listeners
 submit.addEventListener('submit', searchMeal);
-// random.addEventListener('click', getRandomMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
